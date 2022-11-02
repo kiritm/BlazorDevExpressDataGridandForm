6f06517771069b5490f135074063e14b92cf8873
Kirit Maganlal Shah <kiritshah2@yahoo.com.sg>
2022-11-02 22:24:02 +0800

    This is blazor website demo
    Dev Express Blazor dot net core 6 using created DXGrid and separate add new record or edit record form.

---
+++ Data/WeatherForecastService.cs
@@ -0,0 +1,40 @@
+namespace DevExpressDataGridandForm.Data {
+    public class WeatherForecastService {
+        public readonly string[] Summaries = new[] {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private List<WeatherForecast>? Forecasts;
+        public Task<List<WeatherForecast>> GetForecastAsync() {
+            if (Forecasts == null) {
+                var rnd = new Random();
+                Forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast {
+                    ID = index,
+                    Date = DateTime.Today.AddDays(index),
+                    TemperatureC = rnd.Next(-20, 55),
+                    Summary = Summaries[rnd.Next(Summaries.Length)]
+                }).ToList();
+            }
+            return Task.FromResult(Forecasts);
+        }
+        public Task<List<WeatherForecast>> ChangeForecastAsync(WeatherForecast changed) {
+            var orginalForecast = Forecasts.FirstOrDefault(i => i.ID == changed.ID);
+            if (orginalForecast != null) {
+                orginalForecast.TemperatureC = changed.TemperatureC;
+                orginalForecast.Summary = changed.Summary;
+                orginalForecast.Date = changed.Date;
+            }
+            else {
+                Forecasts.Add(changed);
+            }
+            return Task.FromResult(Forecasts);
+        }
+        public Task<List<WeatherForecast>> Remove(WeatherForecast forecast) {
+            Forecasts.Remove(forecast);
+            return Task.FromResult(Forecasts);
+        }
+        public Task<WeatherForecast> GetForecastByIdAsync(int id) {
+            return Task.FromResult(Forecasts.Where(m => m.ID == id).FirstOrDefault());
+        }
+      
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
+﻿///
+Author 
+👋 Hi, I’m Kirit Shah
+👀 I’m interested in technology which involve software, hardware and system.
+🌱 I’m currently learning and practising to sustain my skill set.
+💞️ I’m looking to collaborate on Dotnet core 6, Blazor, MVC, ASP, Microsoft SQL, Embeded Technology
+📫 How to reach me? Call +60166876896 or email: kiritshah234@gmail.com
+\\\
+
+This example use DeveExpress and shows following demo
+DxGrid (DataGrid) and seperate form for add new or edit existing record.
